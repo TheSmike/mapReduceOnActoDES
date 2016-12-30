7b722706ae893f46b17acc57e8e8a623f82d92e2
Michele Scarpenti <michele.scarpenti@omigrade.it>
2016-12-30 12:36:26 +0100

    clean Boot

--- src/it/unipr/aotlab/mapreduce/countword/WordCountMaster.java
+++ src/it/unipr/aotlab/mapreduce/countword/WordCountMaster.java
@@ -3,13 +3,26 @@ package it.unipr.aotlab.mapreduce.countword;
 import it.unipr.aotlab.actodes.actor.Behavior;
 import it.unipr.aotlab.actodes.actor.Binder;
 
-
 public final class WordCountMaster extends Behavior {
-	
+
 	@Override
 	public void initialize(Binder b, Object[] v) {
-		// TODO Auto-generated method stub
-		super.initialize(b, v);
-	}
+		if (!checkInputValidity(v)){
+			System.err.println("Fine programma");
+			return; 
+		}
 	
+		if (((int) v[0] > 0) && ((int) v[1] > 0)) {
+			int n_workers = (int) v[0];
+		}
+	}
+
+	private boolean checkInputValidity(Object[] v) {
+		if (v.length != 4)
+			throw new InitializeException();
+			return false;
+		
+		return true;
+	}
+
 }
