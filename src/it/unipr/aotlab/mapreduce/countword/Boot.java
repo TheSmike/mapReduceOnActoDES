d8a58ca97b3e7d1eaeb3ed0bf26da6e7caf15d4f
Vittaz <vittazz@yahoo.it>
2017-01-04 15:57:19 +0100

    fixed- CountwordMap.java

--- src/it/unipr/aotlab/mapreduce/countword/Boot.java
+++ src/it/unipr/aotlab/mapreduce/countword/Boot.java
@@ -6,19 +6,29 @@ import it.unipr.aotlab.actodes.logging.Logger;
 import it.unipr.aotlab.actodes.logging.TextualFormatter;
 import it.unipr.aotlab.actodes.runtime.Controller;
 import it.unipr.aotlab.actodes.runtime.active.ThreadPoolScheduler;
+import it.unipr.aotlab.mapreduce.Master;
+import it.unipr.aotlab.mapreduce.context.MapJob;
+import it.unipr.aotlab.mapreduce.context.ReduceJob;
+import stub.WaitAndEchoJob;
+import stub.WaitAndEchoReduceJob;
+
 
 public class Boot {
-
 	public static void main(String[] args) {
-		final int workers = 3;	
+		// params
+		final int workers = 3;
 		final int blockSize = 1024;
+		final String inputPath = "resources/CountWord";
+		final String outputPath = "resources/output/";
+		final MapJob mapJob = new CountwordMap();
+		final ReduceJob reduceJob = new WaitAndEchoReduceJob();
+
 		Configuration c = Controller.INSTANCE.getConfiguration();
 		c.setScheduler(ThreadPoolScheduler.class.getName());
-		c.setCreator(WordCountMaster.class.getName());
-		c.setArguments(workers, "inputPath", "outputPath", blockSize);
+		c.setCreator(Master.class.getName());
+		c.setArguments(workers, inputPath, outputPath, blockSize, mapJob, reduceJob);
 		c.setFilter(Logger.ALLLOGS);
 		c.addWriter(ConsoleWriter.class.getName(), TextualFormatter.class.getName(), null);
 		Controller.INSTANCE.run();
 	}
-
 }
