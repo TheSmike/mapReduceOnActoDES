4d6b5c0bec4994ca9ec3646bd813db56b7a1738a
Vittaz <vittazz@yahoo.it>
2017-01-04 17:03:08 +0100

    CountwordReduce.java

--- src/stub/WaitAndEchoReduceJob.java
+++ src/stub/WaitAndEchoReduceJob.java
@@ -1,8 +1,8 @@
 package stub;
 
+import java.util.List;
 import java.util.Random;
 
-import it.unipr.aotlab.mapreduce.context.MapJob;
 import it.unipr.aotlab.mapreduce.context.Context;
 import it.unipr.aotlab.mapreduce.context.ReduceJob;
 
@@ -16,7 +16,7 @@ public class WaitAndEchoReduceJob implements ReduceJob{
 	}
 
 	@Override
-	public void execute(String key, String[] values, Context context) throws Exception {
+	public void execute(String key, List<String> values, Context context) throws Exception {
 		Thread.sleep(1000 + random.nextInt(1000));
 		int nextKey = getNextKey(); 
 		context.put(nextKey, key.substring(0,3));
