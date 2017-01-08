6424ff62c1358a944092025150ecfdae601fc770
Vittaz <vittazz@yahoo.it>
2017-01-08 12:44:08 +0100

    BlockReader and NewLinesReader

--- src/it/unipr/aotlab/mapreduce/resources/BlockReader.java
+++ src/it/unipr/aotlab/mapreduce/resources/BlockReader.java
@@ -3,71 +3,172 @@ package it.unipr.aotlab.mapreduce.resources;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlockReader {
 	private String inputPath; // folder or file path
 	private int totalBlockNumber;
 	private int blockSize;
+	private List<Integer> startPosition_list;
+	protected RandomAccessFile reader;
 
 	//costruttore
-	public BlockReader(String inputPath, int blockSize) {
+	public BlockReader(String inputPath, int blockSize) throws IOException {
 		this.inputPath = inputPath;
 		this.blockSize = blockSize;
+		this.startPosition_list = new ArrayList<Integer>();
 		
 		//conteggio blocchi
 		int dim_file = (int)new File(inputPath).length();
 		
 		
 		int num_blocchi = dim_file/this.blockSize;
-		// fare conteggio dei blocchi
-		// ...
-		//
 
 		this.totalBlockNumber = num_blocchi;
+		
+		File fileToRead = new File(this.inputPath);
+		
+		try {
+			this.reader = new RandomAccessFile(fileToRead, "r");
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		
+		
+		int startPosition = 0;
+		int blocchi_di_scarto = 0;
+		
+		
+	    for ( int i = 0; i < totalBlockNumber; i++)
+	    {
+	    	
+	      boolean scarto = false;
+	    	
+	      reader.seek(startPosition);
+	      
+	      //System.out.println("startposition:"+startPosition);
+	      
+	      if(i == 0)
+	      {
+	      
+	    	startPosition = 0;
+	    	
+	    
+	    	 
+	      }
+	      else
+	      {
+	    	  //System.out.println("startposition senza blocksize = "+startPosition);
+	    	  reader.seek(startPosition+blockSize);
+	    	  
+	    	 
+
+	  		//salvo il puntatore corrente di lettura file
+	  		long pointer = reader.getFilePointer();
+	  		
+	  		if(pointer > fileToRead.length()) 
+	  			{
+	  			pointer = fileToRead.length();
+	  			blocchi_di_scarto++;
+	  			scarto = true;
+	  			reader.seek(pointer);
+	  			}
+	  		
+	  		//System.out.println(pointer);
+	  		
+	  		
+	  		if(pointer != fileToRead.length())
+	  		{
+	  		//tiene conto che non sono a fine riga
+	  		if(reader.readLine().length() != 0)
+	  		{
+	  		reader.seek(pointer-1);
+	  		byte bit = reader.readByte();
+	  		char c = (char) bit;
+	  		if(c != '\n') // controllo di non essere appena a capo
+	  		{
+	  			//System.out.println("sono a fine riga");
+	  			
+	  		
+	  		//ho dei byte da leggere per arrivare a fine riga
+	  		
+	  		reader.seek(pointer);
+	  				
+	  		String line = reader.readLine();
+	  		
+	  		//System.out.println(line);
+	  		
+	  		startPosition = startPosition+blockSize+line.length()+2;
+	  		}
+	  		}
+	  		
+	  		}
+	      }
+	      
+	      if(scarto == false) startPosition_list.add(startPosition);
+	      
+	      
+	    	
+	    } 
+		
+		
+	    for(Integer posizione : startPosition_list)
+	    {
+	    	System.out.print("Startposition blocchi :"+posizione+"\n");
+	    }
+	    
+	    if( blocchi_di_scarto > 0) this.totalBlockNumber -= blocchi_di_scarto;
+		
 	}
 
 	public int getTotalBlockNumber() {
 		return totalBlockNumber;
 	}
 
-	public NewLinesReader getInputLinesReader(int blockNumber) {
+	public NewLinesReader getInputLinesReader(int blockNumber) throws IOException {
 		// todo in base al blockNumber restituisce un oggetto che legge dal byte
 		// X al byte Y oppure al blockSize
-		int inizio_lettura = blockNumber*blockSize;
+		//int inizio_lettura = blockNumber*blockSize;
 		
 		File f = null;
 		
 		f = new File("resources/CountWord/file_parole.txt");
 		
-		return new NewLinesReader(inizio_lettura,blockSize,f);
+		//System.out.println("lunghezza_file:"+f.length());
+		
+		//return new NewLinesReader(blockNumber,blockSize,f);
+		return new NewLinesReader(startPosition_list.get(blockNumber), blockSize, f );
 		
 	}
 	
 	public static void main(String[] args) throws IOException
 	{
-		BlockReader blockreader = new BlockReader("resources/CountWord/file_parole.txt",10);
+		BlockReader blockreader = new BlockReader("resources/CountWord/file_parole.txt",25);
 		
 		int n_blocchi = blockreader.getTotalBlockNumber();
 		
 		System.out.println("Il numero di blocchi in cui spezzo il file è "+n_blocchi+" \n");
 		
-		//for(int i = 0; i < blockreader.totalBlockNumber; i++)
-		//{
-		//genero un nuovo linereader che legge il blocco 3
-		//i = 14 al max perchè parto dal blocco 0
-		 NewLinesReader line_reader = blockreader.getInputLinesReader(14);
+		for(int i = 0; i < blockreader.totalBlockNumber; i++)
+		{
+		
+		 NewLinesReader line_reader = blockreader.getInputLinesReader(i);
 		  
 		  String line;
 		
-		//  System.out.println("iterazione "+i+"\n");
+		  System.out.println("iterazione "+i+"\n");
 		  while ((line = line_reader.readLine()) != null) {
 		    // esegui elaborazione sulla riga
-			  
-			 
+			 System.out.println("readline: ["+line+"]");
+			 //System.out.println(line);
 			
 			
 		  }
-		//}
+		}
+		
+		System.out.println("Esecuzione terminata \n");
 		  
 	}
 }
--- src/it/unipr/aotlab/mapreduce/resources/NewLinesReader.java
+++ src/it/unipr/aotlab/mapreduce/resources/NewLinesReader.java
@@ -11,16 +11,43 @@ public class NewLinesReader {
 	
 	protected RandomAccessFile reader;
 	private File fileToRead;
-	private int startPosition;
+	//private int startPosition;
 	private int blockSize;
+	private int byte_letti;
+	private long pointer;
+	private int blockNumber;
+	private int startPosition;
 	//protected BufferedReader reader;
 	
 	//imposto un line reaer che legge un certo blocco di file
-	public NewLinesReader(int startPosition, int blockSize, File file) {
+	public NewLinesReader(int startPosition, int blockSize, File file) throws IOException {
 		this.fileToRead = file;
 		reader = null;
-		this.startPosition = startPosition;
+		//this.startPosition = startPosition;
 		this.blockSize = blockSize;
+		this.byte_letti = 0;
+		this.pointer = 0;
+		//this.blockNumber = BlockNumber;
+		//this.startPosition = 0;
+		this.startPosition = startPosition;
+		
+		
+		try {
+			reader = new RandomAccessFile(fileToRead, "r");
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		
+		reader.seek(startPosition);
+		
+		
+		/*
+		//dimensione fissa se ciascun blocco iniziasse ad inizio riga
+		//va aggiornata in base alla startposition.
+		int inizio_lettura = blockNumber*blockSize;
+		
+		//System.out.println("startposition iniziale "+inizio_lettura+"\n");
+		
 		//String percorso = "resources/CountWord/file_parole.txt";
 		try {
 			reader = new RandomAccessFile(fileToRead, "r");
@@ -28,14 +55,190 @@ public class NewLinesReader {
 			throw new RuntimeException(e);
 		}
 		
+		//se non sono al primo blocco devo recuperare i byte dell'iterazione precedente
+		if( BlockNumber != 0)
+		{
+			RandomAccessFile find_prev = new RandomAccessFile(fileToRead, "r");
+			
+			find_prev.seek(inizio_lettura);
+			
+			long pointer_prev = find_prev.getFilePointer();
+			
+			System.out.println("il blocco parte dalla posizione "+pointer_prev);
+			
+			System.out.println("Leggo i prossimi 25 byte (dim blocco): \n");
+			
+			byte[] b1 = new byte[25];
+			find_prev.read(b1,0,25);
+			char[] chars1 = new char[b1.length];
+			for(int i=0;i < b1.length;i++){
+				chars1[i]=(char)b1[i];
+				}
+			
+			String str1 = String.valueOf(chars1);
+			
+			System.out.println("blocco intero letto : "+str1);
+			
+			
+			find_prev.seek(inizio_lettura);
+			
+			if(pointer_prev != fileToRead.length())
+			{
+			//tiene conto che non sono a fine riga
+			if(find_prev.readLine().length() != 0)
+			{
+			find_prev.seek(pointer_prev-1);
+			byte bit_prev = find_prev.readByte();
+			char c_prev = (char) bit_prev;
+			if(c_prev == '\n') // controllo di non essere appena a capo
+			{
+				System.out.println("sono a fine riga");
+				
+			}
+			else 
+			{
+			//ho dei byte da leggere per arrivare a fine riga
+			
+			find_prev.seek(pointer_prev);
+					
+			String line_prev = find_prev.readLine();
+			
+			System.out.println("pezzo per finire la riga: ["+line_prev+"]");
+			
+			
+			
+			
+			inizio_lettura = inizio_lettura+line_prev.length();
+			
+			
+			
+			}
+			}
+			
+			}
+		
+			find_prev.close();
+		}
+		
+		
+		System.out.println("Startposition aggiornata: "+inizio_lettura);
+		/*
+		RandomAccessFile find_pos = new RandomAccessFile(fileToRead, "r");
+		
+		find_pos.seek(inizio_lettura+blockSize);
+		
+		
+		
+		//salvo il puntatore corrente di lettura file
+		long pointer = find_pos.getFilePointer();
+		
+		System.out.println(pointer);
+		
+		if(pointer != fileToRead.length())
+		{
+		//tiene conto che non sono a fine riga
+		if(find_pos.readLine().length() != 0)
+		{
+		find_pos.seek(pointer-1);
+		byte bit = find_pos.readByte();
+		char c = (char) bit;
+		if(c == '\n') // controllo di non essere appena a capo
+		{
+			System.out.println("sono a fine riga");
+			this.startPosition = inizio_lettura+blockSize;
+		}
+		else 
+		{
+		//ho dei byte da leggere per arrivare a fine riga
+		
+		find_pos.seek(pointer);
+				
+		String line = find_pos.readLine();
+		
+		System.out.println(line);
+		
+		this.startPosition = inizio_lettura+blockSize+line.length();
+		}
+		}
+		
+		}
+		
+		System.out.println("Se dovessi leggere il blocco cosi come è mi fermerei a "+(inizio_lettura+blockSize));
+		System.out.println("La lettura del prossimo blocco parte da "+this.startPosition+"\n");
+		*/
+		
+		
+		/*
+		if(blockNumber != 0)
+		{
+			startPosition = inizio_lettura+2;
+		}
+		else
+		{
+			startPosition = inizio_lettura;
+		}
+		
+		reader.seek(startPosition);
+		
+		//String prova = reader.readLine();
+		//System.out.println("stringa di prova :["+prova+"]");
+		//find_pos.close();
+		//reader.seek(startPosition);
+		*/
+		
+		//reader.seek(startPosition);
+		
 	}
 
 	//dopo che ho settato il line reader leggo la linea
 	public String readLine() throws IOException {
 		// implementazione del reader
-		// TODO
-		//RandomAccessFile reader2 = new RandomAccessFile(fileToRead, "r");
-		//System.out.println("Bella \n");
+		
+		//mi posiziono all'inizio del blocco e quindi all'inizio della riga (nel caso blocco 0)
+		//reader.seek(startPosition);
+		/*
+		RandomAccessFile find_pos = new RandomAccessFile(fileToRead, "r");
+		find_pos.seek(startPosition);
+		byte bit = find_pos.readByte();
+		char c = (char) bit;
+		System.out.println("carattere: ["+c+"]");
+		if(c == '\n') // controllo di non essere appena a capo
+		{
+			System.out.println("sono a fine riga");
+		}
+		*/
+		if(byte_letti >= blockSize) 
+			{
+			System.out.println("NULL");
+			return null;
+			}
+		
+		String riga = reader.readLine();
+		
+		//pointer = reader.getFilePointer();
+		
+		//System.out.println("puntatore a fine righa: "+pointer+"\n");
+		
+		
+		//reader.seek(pointer);
+		
+		
+		if(riga != null)
+		{
+		byte_letti += (riga.length());
+		}
+		
+		
+		
+			
+		return riga;
+			
+		
+		
+		
+		
+		
+		/*
 		
 		try
 		{
@@ -44,25 +247,23 @@ public class NewLinesReader {
 		//mi posiziono nella posizione definita dal blocco
 		reader.seek(startPosition);
 		
-		//long pointer = reader.getFilePointer();
-		//reader.seek(0);
-		System.out.println(blockSize);
-		//String s = reader.readUTF();
 		
-		//System.out.println(s);
+		System.out.println(blockSize);
+		
+		
+		
 		byte[] b = new byte[blockSize];
 		reader.read(b,0,blockSize);
 		char[] chars = new char[b.length];
 		for(int i=0;i < b.length;i++){
 			chars[i]=(char)b[i];
 			}
-		//char[] chars = new char[blockSize];
-		//reader.read(chars,0,blockSize);
+		
 		String str = String.valueOf(chars);
 		
 		System.out.println(str);
 		
-		//System.out.println(str.length());
+		
 		
 		//salvo il puntatore corrente di lettura file
 		long pointer = reader.getFilePointer();
@@ -70,7 +271,7 @@ public class NewLinesReader {
 		
 		reader.seek(pointer);
 		
-		//System.out.println(reader.readChar());
+	
 		
 		
 		
@@ -111,8 +312,7 @@ public class NewLinesReader {
     		for(int i=0;i < b_resize.length;i++){
     			chars_resize[i]=(char)b_resize[i];
     			}
-    		//char[] chars = new char[blockSize];
-    		//reader.read(chars,0,blockSize);
+    		
     		String str_resize = String.valueOf(chars_resize);
     		
     		System.out.println(str_resize);
@@ -128,33 +328,6 @@ public class NewLinesReader {
 		}//chiusa if != null
 		}
 		
-		   /*
-			String line;
-			//byte[] b = new byte[blockSize];
-			boolean ok = false;
-			int lunghezza = 0;
-			byte[] chunks  = null;
-			
-	        while (ok == false) {
-	            line = reader.readLine();
-	            //lunghezza += line.length();
-	            System.out.println(line+"\n");
-	            System.out.println(lunghezza);
-	            chunks = line.getBytes("UTF-8");
-	            System.out.println(chunks.length);
-	            //reader2.seek(lunghezza);
-	            //System.out.println(reader.readChar());
-	            ok = true;
-	        }
-		*/
-		// read the first byte and print it
-        //System.out.println("" + reader.read());
-        
-		//long pointer = reader.getFilePointer();
-		//leggo un tot di byte
-		//byte[] b = new byte[blockSize];
-		
-		//System.out.println(reader.readUTF());
 		}  
 	    catch(IOException exc) { 
 	      System.out.println("Error seeking or reading."); 
@@ -209,6 +382,6 @@ public class NewLinesReader {
 		
 		
 		
-		return null;
+		//return null;
 	}
 }
